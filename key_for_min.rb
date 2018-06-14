cda45b415a66c8044e8fc73c1898cb45310162bc
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-14 21:57:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
     nil 
   else 
    name_hash.each do |key,value|
-      if lowestv == nil || lowestv > value 
+      if lowestv == nil || value < lowestv
         lowestv = value 
         newkey = key 
       end 
